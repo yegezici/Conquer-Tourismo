2f8fcf9e758729173e8c3d7b978fde4d9ff6b90f
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-12 22:44:18 +0300

    Update BottomPane.java

--- .github/workflows/a/src/BottomPane.java
+++ .github/workflows/a/src/BottomPane.java
@@ -1,26 +1,26 @@
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
-public class BottomPane extends HBox {
-    public BottomPane() {
-    	
-    	setSpacing(250);
-    	Button b = new Button("DRIVE");
-		b.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-font-size: 24px; -fx-font-family: Arial;"
-				+ "-fx-font-style: italic;");
-        b.setOnMouseEntered(e -> b.setOpacity(1.0));
-        b.setOnMouseExited(e -> b.setOpacity(0.5));
-         //çalışmasını görmek için yazdım
-        b.setOnAction(e -> {
-            System.out.println("Button clicked!");
-        });
-        //
-    	setPadding(new Insets(15,0,150,15));
-    	getChildren().addAll(new Label("Sample Text\nSample New Line"), b);
-    	
-    }
+public class BottomPane extends BorderPane {
+	public BottomPane() {
+
+		Button b = new Button("DRIVE");
+		b.setStyle(
+				"-fx-background-color: transparent; -fx-border-color: transparent; -fx-font-size: 24px; -fx-font-family: Arial;"
+						+ "-fx-font-style: italic;");
+		setStyle("-fx-border-color: black");
+		b.setOnMouseEntered(e -> b.setOpacity(1.0));
+		b.setOnMouseExited(e -> b.setOpacity(0.5));
+		// çalışmasını görmek için yazdım
+		b.setOnAction(e -> {
+			System.out.println("Button clicked!");
+		});
+		//
+
+		setPadding(new Insets(15, 0, 150, 15));
+		setRight(b);
+		setLeft(new Text("SADSA"));
+	}
 }
