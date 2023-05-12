413047a4981b5e3a3c404bf3d4a9e997b262e628
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-12 21:31:52 +0300

    Update BottomPane.java

--- .github/workflows/a/src/BottomPane.java
+++ .github/workflows/a/src/BottomPane.java
@@ -10,8 +10,16 @@ public class BottomPane extends HBox {
     	
     	setSpacing(250);
     	Button b = new Button("DRIVE");
+		b.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-font-size: 24px; -fx-font-family: Arial;"
+				+ "-fx-font-style: italic;");
+        b.setOnMouseEntered(e -> b.setOpacity(1.0));
+        b.setOnMouseExited(e -> b.setOpacity(0.5));
+         //çalışmasını görmek için yazdım
+        b.setOnAction(e -> {
+            System.out.println("Button clicked!");
+        });
+        //
     	setPadding(new Insets(15,0,150,15));
-    	b.setFont(Font.font("Arial" , FontWeight.BOLD , 24));
     	getChildren().addAll(new Label("Sample Text\nSample New Line"), b);
     	
     }
