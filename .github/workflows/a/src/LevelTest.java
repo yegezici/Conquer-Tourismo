cf4a5ea05f49d9cc8ed4076a9f3ef35004e82066
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-13 21:58:42 +0300

    Delete LevelTest.java

--- .github/workflows/a/src/LevelTest.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-
-public class LevelTest {
-	public static void main(String[] args) throws FileNotFoundException {
-		File name = new File("level1.txt");
-		NewLevel level = new NewLevel(name);
-		level.readingFile();
-	}
-}
