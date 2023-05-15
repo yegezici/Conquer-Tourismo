ebd4fa517ad8abc379fad2eecc38191d3f30f15d
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2023-05-16 00:54:36 +0300

    Update Main.java

--- .github/workflows/a/src/Main.java
+++ .github/workflows/a/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,12 +19,17 @@ public class Main extends Application {
         BorderPane br = new BorderPane();
         TopPane tp = new TopPane();
         BottomPane bp = new BottomPane();
+        
         String[] lvlarr = {"level1.txt","level2.txt","level3.txt","level4.txt","level5.txt"};
-		File name = new File(lvlarr[0]);
-		NewLevel level = new NewLevel(name);
-		level.readingFile();							
+    	File name = new File(lvlarr[0]);
+    	NewLevel level = new NewLevel(name);
+		level.readingFile();	
 		CenterPane root = new CenterPane(level);
+		
+		
+		
         br.setCenter(root);
+        root.setAlignment(Pos.CENTER);
         br.setTop(tp);
         br.setBottom(bp);
 
