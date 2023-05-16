62fcef71711151719286d8bd2bfa9c5430da890a
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-16 14:41:32 +0300

    Update CityButton.java

--- .github/workflows/a/src/CityButton.java
+++ .github/workflows/a/src/CityButton.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -13,6 +14,8 @@ import javafx.scene.layout.VBox;
 public class CityButton {
 	String name;
 	Image img;
+	ArrayList<Integer> rowLinePoints = new ArrayList<>();
+	ArrayList<Integer> colLinePoints = new ArrayList<>();
 	static ArrayList<Integer> points = new ArrayList<Integer>();
 	int colIndex = 0, rowIndex = 0;
 	int endColIndex = 0, endRowIndex = 0;
@@ -27,68 +30,89 @@ public class CityButton {
 		this.img = img;
 		this.colIndex = colIndex;
 		this.rowIndex = rowIndex;
-		
-		
+
 	}
 
 	public BorderPane cityButton(GridPane pane) {
 		Button cB = new Button();
 		cB.setStyle("-fx-background-color: transparent; -fx-border-color: transparent;");
-		
-		
+
 		cB.setOnAction(e -> {
 			// row index aslinda y degeri o yuzden ilk colu almamiz laizm yerlerini
 			// degistirdim
-			points.add(colIndex-1);
-			points.add(rowIndex-1);
-			
-			for(int i = 0; i < points.size(); i++)
-				System.out.print(points.get(i) +"-");
+			points.add(colIndex - 1);
+			points.add(rowIndex - 1);
 			
 			
-				
-			
-			
+
+
+			for (int i = 0; i < points.size(); i++)
+				System.out.print(points.get(i) + "-");
+
 			if (points.size() == 4) {
 				if ((points.get(0) > points.get(2))) {
 					bRI = points.get(0);
 					sRI = points.get(2);
-					
-				}else {
+
+				} else {
 					bRI = points.get(2);
 					sRI = points.get(0);
 				}
 				if ((points.get(1) > points.get(3))) {
 					bCI = points.get(1);
 					sCI = points.get(3);
-					
-				}else {
+
+				} else {
 					bCI = points.get(3);
 					sCI = points.get(1);
 				}
-						for (int i = bRI - 1; i >= sRI; i--) {
-							Direction d = new Direction();
-							pane.add(d.createRowLine(), i, points.get(1));
-						}
-						for (int i = sCI ; i < bCI ; i++) {
-							Direction d = new Direction();
-							pane.add(d.createColLine(), points.get(2), i);		
-						}
-						
-						System.out.println(calculateDistance(points.get(0), points.get(1), points.get(2), points.get(3)));
-						 points.clear();
-						
-					}
-				
-				
-			
-				  
-		
+				for (int i = bRI - 1; i >= sRI; i--) {
+					Direction d = new Direction();
+					pane.add(d.createRowLine(), i, points.get(1));
+					rowLinePoints.add(i);
+					rowLinePoints.add(points.get(1));
+				}
+				for (int i = sCI; i < bCI; i++) {
+					Direction d = new Direction();
+					pane.add(d.createColLine(), points.get(2), i);
+					colLinePoints.add(points.get(2));
+					colLinePoints.add(i);
+				}
 
-			
+				System.out.println(calculateDistance(points.get(0), points.get(1), points.get(2), points.get(3)));
+			}
+
+			if (points.size() == 6) {
+				ArrayList<Node> nodesToRemove = new ArrayList<>();
+				for (int i = 0; i < rowLinePoints.size(); i++) {
+					for (Node n : pane.getChildren()) {
+						if (GridPane.getRowIndex(n) == rowLinePoints.get(1)
+								&& GridPane.getColumnIndex(n) == rowLinePoints.get(i)) {
+							nodesToRemove.add(n);
+						}
+					}
+				}
+				for (int i = 0; i < colLinePoints.size(); i++) {
+					for (Node n : pane.getChildren()) {
+						if (GridPane.getRowIndex(n) == colLinePoints.get(i)
+								&& GridPane.getColumnIndex(n) == colLinePoints.get(1)) {
+							nodesToRemove.add(n);
+						}
+					}
+				}
+				for (Node node : nodesToRemove) {
+					pane.getChildren().remove(node);
+				}
+
+				int x = points.get(4);
+				int y = points.get(5);
+				points.clear();
+				points.add(x);
+				points.add(y);
+			}
+
 		});
-	
-		
+
 		Label label = new Label("  " + this.name);
 		label.setAlignment(Pos.BOTTOM_CENTER);
 
