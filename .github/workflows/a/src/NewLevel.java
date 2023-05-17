87c36939461c5cbabc39de81d1bc8e53a8ec3d5a
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-18 00:22:21 +0300

    Update NewLevel.java

--- .github/workflows/a/src/NewLevel.java
+++ .github/workflows/a/src/NewLevel.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NewLevel {
-	File filename;
+	File file;
+	String nameOfFile;
+	int score;
 	int fixedCell;
 	Vehicle vehicle = new Vehicle();
 	Passenger passanger = new Passenger();
@@ -13,20 +15,22 @@ public class NewLevel {
     ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
     ArrayList<Passenger> passengers = new ArrayList<Passenger>();
     ArrayList<Integer> fixedCells = new ArrayList<Integer>();
+    private CenterPane centerpane;
+	
     
-	NewLevel() {
+    NewLevel() {
 
 	}
 
-	NewLevel(File filename) {
-		this.filename = filename;
-		
+	NewLevel(File file) {
+		this.file = file;
+		nameOfFile = file.getName();
 	}
 	//Scanner boşluk görmediği için her satırı kelime olarak algılıyordu, ben de direkt nextLine ile diğer satırı String olarak alıp
 	//split ile array oluşturdum. 
 	public void readingFile() {
 		try {
-			Scanner input = new Scanner(filename); 
+			Scanner input = new Scanner(file); 
 			String str ="";
 			
 			while (input.hasNextLine()) {
@@ -34,12 +38,12 @@ public class NewLevel {
 				 str = input.nextLine();
 				String[] inputs = str.split(",");
 				if (inputs[0].equals("City")) {
-					city = new City(inputs[1],Integer.valueOf(inputs[2]),Integer.valueOf(inputs[3]));
+					city = new City(inputs[1],Integer.valueOf(inputs[2]),Integer.valueOf(inputs[3]),this);
 					cities.add(city);
 					
 				}
 				else if (inputs[0].equals("Passenger")) {
-					passanger = new Passenger(Integer.valueOf(inputs[1]), Integer.valueOf(inputs[2]),Integer.valueOf(inputs[3]));
+					passanger = new Passenger(Integer.valueOf(inputs[1]), Integer.valueOf(inputs[2]),Integer.valueOf(inputs[3]),this);
 					passengers.add(passanger);
 				}
 				else if (inputs[0].equals("Vehicle")) {
@@ -60,5 +64,12 @@ public class NewLevel {
 			System.out.println("There is no file " + e.getMessage());
 		}
 		
+	}
+
+	public void setCenterPane(CenterPane centerPane) {
+		this.centerpane = centerPane;
+	}
+	public CenterPane getCenterPane() {
+		return this.centerpane;
 	}
 }
