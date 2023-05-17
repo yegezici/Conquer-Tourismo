051b5dc48559f7bab4ead85d9ba6f45654340582
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-17 15:43:30 +0300

    Update BottomPane.java

--- .github/workflows/a/src/BottomPane.java
+++ .github/workflows/a/src/BottomPane.java
@@ -1,26 +1,59 @@
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class BottomPane extends BorderPane {
-	public BottomPane() {
-
-		Button b = new Button("DRIVE");
+	NewLevel level;
+	IntegerProperty propertyValue = new SimpleIntegerProperty(0);
+	
+	public BottomPane(NewLevel level) {
+		this.level = level;
+		DriveButton b = new DriveButton(level);
+		b.setText("DRIVE");
 		b.setStyle(
 				"-fx-background-color: transparent; -fx-border-color: transparent; -fx-font-size: 24px; -fx-font-family: Arial;"
 						+ "-fx-font-style: italic;");
 		setStyle("-fx-border-color: black");
 		b.setOnMouseEntered(e -> b.setOpacity(1.0));
 		b.setOnMouseExited(e -> b.setOpacity(0.5));
-		// çalışmasını görmek için yazdım
-		b.setOnAction(e -> {
-			System.out.println("Button clicked!");
-		});
-		//
-
-		setPadding(new Insets(5,5,5,5));
+		b.setOnAction(new DriveSetOnAction());
+		Text score = new Text("score");
+		score.setTextAlignment(TextAlignment.CENTER);
 		setRight(b);
-		setLeft(new Text("SADSA"));
+		
+		// setLeft();
+ 
+        Label propertyLabel = new Label();
+        propertyLabel.textProperty().bind(Bindings.format("Property Value: %2d",propertyValue));
+        setTop(propertyLabel);
+        
+	
+	}
+
+	public void changeLoc(NewLevel level) {
+		level.getCenterPane().setVx(0);
+		level.getCenterPane().setVy(0);
+	}
+
+	class DriveSetOnAction implements EventHandler<ActionEvent> {
+		@Override
+		public void handle(ActionEvent event) {
+			level.getCenterPane().getChildren().remove(level.getCenterPane().imageView);
+			level.getCenterPane().add(level.getCenterPane().imageView, CityButton.getNextCity().colIndex - 1,
+					CityButton.getNextCity().rowIndex - 1);
+			propertyValue = level.getCenterPane().getForNextCity().loc.calculateScore();
+			System.out.println(propertyValue);
+			Label propertyLabel = new Label();
+	        propertyLabel.textProperty().bind(Bindings.format("Property Value: %2d",propertyValue));
+	        setTop(propertyLabel);
+		}
 	}
 }
