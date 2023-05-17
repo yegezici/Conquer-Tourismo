ff468dbdf388b042f4480048893b2639e3d96ab0
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-18 00:36:35 +0300

    Update CenterPane.java

--- .github/workflows/a/src/CenterPane.java
+++ .github/workflows/a/src/CenterPane.java
@@ -1,23 +1,27 @@
 
-import java.util.ArrayList;
-
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class CenterPane extends GridPane {
 	int rectangleCount = 10;
-
+	private int vx;
+	private int vy;
+	private CityButton forNextCity;
+	ImageView imageView = new ImageView();
+	NewLevel level = new NewLevel();
+	
+	public CenterPane() {
+		
+	}
+	
 	public CenterPane(NewLevel lvl) {
+		level = lvl;
 		int id = 1;
-
+		
 		for (int row = 0; row < rectangleCount; row++) {
 			for (int col = 0; col < rectangleCount; col++) {
 				Rectangle square = new Rectangle();
@@ -25,7 +29,8 @@ public class CenterPane extends GridPane {
 				square.heightProperty().bind(heightProperty().divide(10).subtract(10));
 				;
 				// RENKSIZ OLMASI ICIN YAZDIM
-				square.setFill(Color.TRANSPARENT);
+				square.setFill(Color.YELLOW);
+				square.setStroke(Color.BLACK);
 				square.setId(String.valueOf(id));
 
 				add(square, col, row);
@@ -59,18 +64,18 @@ public class CenterPane extends GridPane {
 					add(iv, (lvl.fixedCells.get(b) % 10) - 1, (lvl.fixedCells.get(b)) / 10);
 					b++;
 				}
-				// GALIBA BIR HATA VAR
 				if (lvl.cities.get(a).getLocId() == i) { // koordinatların tam olması için 1 den çıkartmıyoruz
 					CityButton cB = new CityButton(lvl.cities.get(a).getName(), imgarr[(int) (Math.random() * 6)],
 							(lvl.cities.get(a).getLocId() % 10 == 0 ? (lvl.cities.get(a).getLocId() % 10) + 8
 									: (lvl.cities.get(a).getLocId() % 10)),
 							(lvl.cities.get(a).getLocId() % 10 == 0) ? (lvl.cities.get(a).getLocId() / 10)
 									: (lvl.cities.get(a).getLocId()) / 10 + 1);
+					forNextCity = cB;
 					if (lvl.cities.get(a).getLocId() % 10 == 0) {
-						add(cB.cityButton(), (lvl.cities.get(a).getLocId() % 10) + 9,
+						add(cB.cityButton(this), (lvl.cities.get(a).getLocId() % 10) + 9,
 								(lvl.cities.get(a).getLocId() / 10) - 1);
 					} else {
-						add(cB.cityButton(), (lvl.cities.get(a).getLocId() % 10) - 1,
+						add(cB.cityButton(this), (lvl.cities.get(a).getLocId() % 10) - 1,
 								(lvl.cities.get(a).getLocId() / 10));
 					}
 					row = 0;
@@ -86,30 +91,42 @@ public class CenterPane extends GridPane {
 		createVehicle(lvl);
 	}
 
-	public void createVehicle(NewLevel lvl) {
-        Vehicle vehicle = lvl.vehicles.get(0);
-        Image car ;
-        ImageView imageView = new ImageView();
-        if(lvl.vehicles.get(0).getCapacity() <= 5) {
-        car = new Image("minivan.png");
-        imageView = new ImageView(car);}
-        else if(lvl.vehicles.get(0).getCapacity() > 5 && lvl.vehicles.get(0).getCapacity() < 14) {
-        car = new Image("car.png");
-        imageView = new ImageView(car);}
-        else {
-        car = new Image("bus.png");
-        imageView = new ImageView(car);
-        }
 
-        imageView.setFitWidth(35);
-        imageView.setFitHeight(35);
-        int location = 0;
-        for (int i = 0; i < lvl.cities.size(); i++) {
-            if (vehicle.getCityId() == lvl.cities.get(i).getId())
-                location = lvl.cities.get(i).getLocId();
-        }
-        int vx = location % 10 - 1;
-        int vy = location / 10;
-        add(imageView,vx,vy);
+
+	public void createVehicle(NewLevel lvl) {
+		Vehicle vehicle = lvl.vehicles.get(0);
+		Image car;	
+		if (lvl.vehicles.get(0).getCapacity() <= 5) {
+			car = new Image("minivan.png");
+			imageView = new ImageView(car);
+		} else if (lvl.vehicles.get(0).getCapacity() > 5 && lvl.vehicles.get(0).getCapacity() < 14) {
+			car = new Image("car.png");
+			imageView = new ImageView(car);
+		} else {
+			car = new Image("bus.png");
+			imageView = new ImageView(car);
+		}
+
+		imageView.setFitWidth(35);
+		imageView.setFitHeight(35);
+		int location = 0;
+		for (int i = 0; i < lvl.cities.size(); i++) {
+			if (vehicle.getCityId() == lvl.cities.get(i).getId())
+				location = lvl.cities.get(i).getLocId();
+		}
+		vx = location % 10 - 1;
+		vy = location / 10;
+		add(imageView, vx, vy);
+	}
+
+	public void setVx(int vx) {
+		this.vx = vx;
+	}
+
+	public void setVy(int vy) {
+		this.vy = vy;
+	}
+	public CityButton getForNextCity() {
+		return forNextCity;
 	}
 }
