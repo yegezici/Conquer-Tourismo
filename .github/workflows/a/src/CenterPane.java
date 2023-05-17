a31bf4c074e147dfacb3e7133f2e1e9323857a1b
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2023-05-18 01:00:58 +0300

    Update CenterPane.java

--- .github/workflows/a/src/CenterPane.java
+++ .github/workflows/a/src/CenterPane.java
@@ -1,44 +1,42 @@
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class CenterPane extends GridPane {
 	int rectangleCount = 10;
+    CityButton cB = new CityButton();
+	CityButton[] buttons;
+	Rectangle square = new Rectangle();
+	NewLevel level = new NewLevel();
 	private int vx;
 	private int vy;
 	private CityButton forNextCity;
 	ImageView imageView = new ImageView();
-	NewLevel level = new NewLevel();
 	
 	public CenterPane() {
 		
 	}
-	
+
 	public CenterPane(NewLevel lvl, Text botText) {
-		level = lvl;
 		int id = 1;
-		
+		buttons = new CityButton[lvl.cities.size()];	
 		for (int row = 0; row < rectangleCount; row++) {
 			for (int col = 0; col < rectangleCount; col++) {
-				Rectangle square = new Rectangle();
+				square = new Rectangle();
 				square.widthProperty().bind(widthProperty().divide(10).subtract(10));
 				square.heightProperty().bind(heightProperty().divide(10).subtract(10));
 				;
 				// RENKSIZ OLMASI ICIN YAZDIM
-				square.setFill(Color.YELLOW);
-				square.setStroke(Color.BLACK);
+				square.setFill(Color.TRANSPARENT);
+				//square.setStroke(Color.BLACK);
 				square.setId(String.valueOf(id));
-
 				add(square, col, row);
-
 				id++;
 			}
 		}
-
 		Image img = new Image("ist.png");
 		Image img1 = new Image("mersin.png");
 		Image img2 = new Image("city1.png");
@@ -51,74 +49,71 @@ public class CenterPane extends GridPane {
 		Image bus = new Image("bus.png");
 		Image[] imgarr = { img, img1, img2, img3, img4, img5 };
 		int rectangleCount = 10;
-		int i = 0, a = 0, b = 0;
+	    int i = 0, a=0, b=0,index=0;
 		for (int row = 0; row < rectangleCount; row++) {
 			for (int col = 0; col < rectangleCount; col++) {
-
-				if (a == lvl.cities.size())
-					break;
-				if (!(lvl.fixedCells.size() == b)) {
-					ImageView iv = new ImageView(img6);
-					iv.setFitWidth(35);
-					iv.setFitHeight(35);
-					add(iv, (lvl.fixedCells.get(b) % 10) - 1, (lvl.fixedCells.get(b)) / 10);
-					b++;
-				}
-				if (lvl.cities.get(a).getLocId() == i) { // koordinatların tam olması için 1 den çıkartmıyoruz
-					CityButton cB = new CityButton(lvl.cities.get(a).getName(), imgarr[(int) (Math.random() * 6)],
-							(lvl.cities.get(a).getLocId() % 10 == 0 ? (lvl.cities.get(a).getLocId() % 10) + 8
-									: (lvl.cities.get(a).getLocId() % 10)),
-							(lvl.cities.get(a).getLocId() % 10 == 0) ? (lvl.cities.get(a).getLocId() / 10)
-									: (lvl.cities.get(a).getLocId()) / 10 + 1);
-					forNextCity = cB;
-					if (lvl.cities.get(a).getLocId() % 10 == 0) {
-						add(cB.cityButton(this, botText), (lvl.cities.get(a).getLocId() % 10) + 9,
-								(lvl.cities.get(a).getLocId() / 10) - 1);
-					} else {
-						add(cB.cityButton(this, botText), (lvl.cities.get(a).getLocId() % 10) - 1,
-								(lvl.cities.get(a).getLocId() / 10));
-					}
-					row = 0;
-					col = 0;
-					a++;
-					i = 0;
-
-				} else
-					i++;
+				
+			   if(a==lvl.cities.size()) 
+				   break;
+			   if(!(lvl.fixedCells.size() == b)) {
+				   ImageView iv = new ImageView(img6);
+				   iv.setFitWidth(35);
+				   iv.setFitHeight(35);
+				   add(iv, (lvl.fixedCells.get(b)%10) - 1, (lvl.fixedCells.get(b))/10);
+		           b++;
+		        }
+		        if(lvl.cities.get(a).getLocId() == i ) {  //koordinatların tam olması için 1 den çıkartmıyoruz
+		        CityButton cB = new CityButton(lvl.cities.get(a).getName(), imgarr[(int) (Math.random() * 6)],
+		        		(lvl.cities.get(a).getLocId()%10==0 ? (lvl.cities.get(a).getLocId()%10) + 8 : (lvl.cities.get(a).getLocId()%10)),
+		        		(lvl.cities.get(a).getLocId()%10 == 0) ? (lvl.cities.get(a).getLocId()/10)  : (lvl.cities.get(a).getLocId())/10+1, 
+		        		 lvl, a);
+		       buttons[index++] = cB;
+		       
+		           
+				if(lvl.cities.get(a).getLocId()%10 == 0) {
+					add(cB.cityButton(this, botText), (lvl.cities.get(a).getLocId()%10) +9, (lvl.cities.get(a).getLocId()/10)-1);
+				}else {
+		        add(cB.cityButton(this, botText), (lvl.cities.get(a).getLocId()%10)- 1, (lvl.cities.get(a).getLocId()/10));
+		       
+				}row = 0;
+		        col = 0;
+		        a++;
+		        i = 0;
+		        }
+		        else
+		        i++;
 			}
-
-		}
-		createVehicle(lvl);
+			createVehicle(lvl);
+     
 	}
-
-
-
+		
+	}
 	public void createVehicle(NewLevel lvl) {
 		Vehicle vehicle = lvl.vehicles.get(0);
-		Image car;	
-		if (lvl.vehicles.get(0).getCapacity() <= 5) {
-			car = new Image("minivan.png");
-			imageView = new ImageView(car);
-		} else if (lvl.vehicles.get(0).getCapacity() > 5 && lvl.vehicles.get(0).getCapacity() < 14) {
-			car = new Image("car.png");
-			imageView = new ImageView(car);
-		} else {
-			car = new Image("bus.png");
-			imageView = new ImageView(car);
+		Image car ;
+		ImageView imageView = new ImageView();
+		if(lvl.vehicles.get(0).getCapacity() <= 5) {
+		car = new Image("minivan.png");
+		imageView = new ImageView(car);}
+		else if(lvl.vehicles.get(0).getCapacity() > 5 & lvl.vehicles.get(0).getCapacity() <= 14) {
+		car = new Image("car.png");
+		imageView = new ImageView(car);}
+		else {
+		car = new Image("bus.png");
+		imageView = new ImageView(car);	
 		}
-
-		imageView.setFitWidth(35);
-		imageView.setFitHeight(35);
+		
+		imageView.setFitWidth(30);
+		imageView.setFitHeight(30);
 		int location = 0;
 		for (int i = 0; i < lvl.cities.size(); i++) {
 			if (vehicle.getCityId() == lvl.cities.get(i).getId())
 				location = lvl.cities.get(i).getLocId();
 		}
-		vx = location % 10 - 1;
+	    vx = location % 10 - 1;
 		vy = location / 10;
-		add(imageView, vx, vy);
+		add(imageView,vx,vy);
 	}
-
 	public void setVx(int vx) {
 		this.vx = vx;
 	}
@@ -129,4 +124,5 @@ public class CenterPane extends GridPane {
 	public CityButton getForNextCity() {
 		return forNextCity;
 	}
-}
+}	
+	
