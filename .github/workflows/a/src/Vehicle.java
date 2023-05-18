435ef0702ba93f2fcd2c7f96b553fc8584affb7f
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2023-05-19 01:27:50 +0300

    Update Vehicle.java

--- .github/workflows/a/src/Vehicle.java
+++ .github/workflows/a/src/Vehicle.java
@@ -1,11 +1,11 @@
-import java.awt.Image;
-import java.io.File;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Vehicle {
 	private int cityId;
 	private int capacity;
+	ImageView imageView;
 
 	public Vehicle() {
 
@@ -14,6 +14,20 @@ public class Vehicle {
 	public Vehicle(int cityId, int capacity) {
 		this.cityId = cityId;
 		this.capacity = capacity;
+		Image car;	
+		if (this.capacity <= 5) {
+			car = new Image("minivan.png");
+			imageView = new ImageView(car);
+		} else if (this.capacity > 5 && this.capacity < 14) {
+			car = new Image("car.png");
+			imageView = new ImageView(car);
+		} else {
+			car = new Image("bus.png");
+			imageView = new ImageView(car);
+		}
+
+		imageView.setFitWidth(35);
+		imageView.setFitHeight(35);
 	}
 
 	public int getCityId() {
