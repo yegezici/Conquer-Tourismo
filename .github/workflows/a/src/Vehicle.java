68071b05ceebb6403bcfddb13290f42f78ee87c8
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-18 23:20:54 +0300

    Update Vehicle.java

--- .github/workflows/a/src/Vehicle.java
+++ .github/workflows/a/src/Vehicle.java
@@ -1,23 +1,30 @@
+import java.awt.Image;
+import java.io.File;
+
+import javafx.scene.image.ImageView;
 
 public class Vehicle {
 	private int cityId;
 	private int capacity;
-    
-   public Vehicle() {
-	   
-   }
 
-public Vehicle(int cityId, int capacity) {
-	this.cityId = cityId;
-	this.capacity = capacity;
-}
+	public Vehicle() {
 
-public int getCityId() {
-	return cityId;
-}
+	}
 
-public int getCapacity() {
-	return capacity;
-}
-   
+	public Vehicle(int cityId, int capacity) {
+		this.cityId = cityId;
+		this.capacity = capacity;
+	}
+
+	public int getCityId() {
+		return cityId;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public ImageView createVehicle() {
+		return null;
+	}
 }
