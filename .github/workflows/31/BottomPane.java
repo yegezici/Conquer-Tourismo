a85bfaa2aacf14f05e47170dc69e131fe0f489d9
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-21 00:37:14 +0300

    Update BottomPane.java

--- .github/workflows/31/BottomPane.java
+++ .github/workflows/31/BottomPane.java
@@ -77,21 +77,12 @@ public class BottomPane extends BorderPane {
 				line = polyline;
 			}
 			pane.animation(polyline);
-			lvl.getPane().top.score = new SimpleIntegerProperty(calculateScore(lvl.getPane().center.city.
-					calculateDistance((int)(startX), (int)(startY), (int)(endX), (int)(endY))));
-			Label propertyLabel = new Label();
-	        propertyLabel.textProperty().bind(Bindings.format("Property Value: %2d",lvl.getPane().top.score));
-	        System.out.println("buton");
-	        setTop(propertyLabel);
+			lvl.getPane().top.score.set(lvl.getPane().center.city.calculateScore(lvl.getPane().center.city.calculateDistance((int)(startX),
+					(int)(startY),(int)(endX),(int)(endY)))); 
+	        lvl.getPane().top.scoreLabel.textProperty().bind(Bindings.format("Score: %d" ,lvl.getPane().top.score));
+	   
 			
 		}
 	}
-	
-	public int calculateScore(int distance) {
-		int cost = distance;
-		int income = (int) (lvl.vehicle.pasAtVehicle * distance * 0.2);
-		return income - cost;
-	}
-
 
 }
