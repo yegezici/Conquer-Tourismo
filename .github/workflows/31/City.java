74f9281ab0667ca02ba99ac802915aa1cdc94fcb
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2023-05-25 01:04:46 +0300

    Add files via upload

---
+++ .github/workflows/31/City.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class City {
+	
+	private String name;  //name represents name of this city.
+	private int locId;    //locId represent location id in the center pane of this city. For example, 5 means first row and fifth column.
+	private int id;       //id represents specific id in this level.
+	
+
+	
+	//No-arg constructor for city class.
+	public City() {
+    
+	}
+    //This constructor takes parameter as name, locId, id and level.
+	public City(String name, int locId, int id) {
+		this.id = id;
+		this.name = name;
+		this.locId = locId;
+		
+		
+	}
+    //Getter methods for name, locId, and id.
+	//There are only getter methods because this data fields is constant and cannot be changed by user.
+	public String getName() {
+		return name;
+	}
+
+	public int getLocId() {
+		return locId;
+	}
+
+	public int getId() {
+		return id;
+	}
+}
