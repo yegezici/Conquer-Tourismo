dbab1fe3f937e3206fd7efed218950fc744357e0
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2023-05-25 02:13:02 +0300

    Update Passenger.java

--- .github/workflows/31/Passenger.java
+++ .github/workflows/31/Passenger.java
@@ -1,23 +1,27 @@
 import java.util.ArrayList;
 
 public class Passenger {
-	private int numOfPas;
-	private int startId;
-	private int destId;
-	ArrayList<Passenger> allOfPas = new ArrayList<>();
-	NewLevel level;
+	private int numOfPas;                   // It stores the number of passengers in this object.
+	private int startId;                    // It stores the ID of the starting city for the passengers in this object.
+	private int destId;                     // It stores the ID of the destination city for the passengers in this object.
+	ArrayList<Passenger> allOfPas;  	    // It represents all the passengers in the current level.
+	NewLevel level;                 		// It represents a NewLevel object.
+	
 	public Passenger() {
-
+       //No-arg constructor     
 	}
-
+           
+	//It construct passenger object with provided with specified data fields and add this object to allOfPas array list.
 	public Passenger(int numOfPas, int startId, int destId, NewLevel level) {
 		this.numOfPas = numOfPas;
 		this.startId = startId;
 		this.destId = destId;
 		this.level = level;
 		allOfPas.add(this);
+		
 	}
-
+    
+	//GETTER AND SETTER METHODS OF DATA FIELD IN THIS OBJECT.
 	public int getNumOfPas() {
 		return numOfPas;
 	}
@@ -33,5 +37,5 @@ public class Passenger {
 	public void setNumOfPas(int numOfPas) {
 		this.numOfPas = numOfPas;
 	}
- 
+    
 }
