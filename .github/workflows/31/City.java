2d286bd9e83a54eeef8d2d3425be0e63f0f321f6
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-26 01:12:30 +0300

    Update City.java

--- .github/workflows/31/City.java
+++ .github/workflows/31/City.java
@@ -1,78 +1,36 @@
-
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-public class Vehicle {
-	private int cityId;               //citId keeps in which city this vehicle is placed first.
-	private int destCityId;           //destCityId keeps the ID of the destination city to which the vehicle is traveling.
-	private int capacity;             //capacity keeps the maximum number of capacity to transport.
-	ImageView imageView;              //imageView is used to show the image of this vehicle in scene.
-	double xCordinate;                //x coordinate of this vehicle.
-	double yCordinate;                //y coordinate of this vehicle.    
-	int pasAtVehicle;				  //It keeps how many passengers left in this vehicle.		             
-	private String CityName;          //Name of the city this vehicle is placed in.
+//Muhammed Hasan Erzincanli 150121031
+//Yunus Emre Gezici 150121066
+public class City {
 	
+	private String name;  //name represents name of this city.
+	private int locId;    //locId represent location id in the center pane of this city. For example, 5 means first row and fifth column.
+	private int id;       //id represents specific id in this level.
 	
+
 	
-	//No-arg constructor for this class.
-	public Vehicle() {
-
+	//No-arg constructor for city class.
+	public City() {
+    
 	}
-	//Vehicle() constructor takes cityId and capacity as parameters.
-	public Vehicle(int cityId, int capacity) {
-	    this.cityId = cityId;
-	    this.capacity = capacity;
-	    
-	    Image car;
-
-	    // Determine the appropriate image based on the vehicle's capacity
-	    if (this.capacity <= 5) {
-	        car = new Image("car.png");
-	        imageView = new ImageView(car);
-	    } else if (this.capacity > 5 && this.capacity < 14) {
-	        car = new Image("minivan.png");
-	        imageView = new ImageView(car);
-	    } else {
-	        car = new Image("bus.png");
-	        imageView = new ImageView(car);
-	    }
-        //Set imageView's width and height to appropriate sizes.
-	    imageView.setFitWidth(25);    
-	    imageView.setFitHeight(25);
+    //This constructor takes parameter as name, locId, id and level.
+	public City(String name, int locId, int id) {
+		this.id = id;
+		this.name = name;
+		this.locId = locId;
+		
+		
+	}
+    //Getter methods for name, locId, and id.
+	//There are only getter methods because this data fields is constant and cannot be modified afterward.
+	public String getName() {
+		return name;
 	}
 
-	// Getter method for cityId
-	public int getCityId() {
-	    return cityId;
+	public int getLocId() {
+		return locId;
 	}
 
-	// Getter method for capacity
-	public int getCapacity() {
-	    return capacity;
-	}
-
-	// Setter method for cityId
-	public void setCityId(int cityId) {
-	    this.cityId = cityId;
-	}
-
-	// Getter method for destCityId
-	public int getDestCityId() {
-	    return destCityId;
-	}
-
-	// Setter method for destCityId
-	public void setDestCityId(int destCityId) {
-	    this.destCityId = destCityId;
-	}
-
-	// Getter method for CityName
-	public String getCityName() {
-	    return CityName;
-	}
-
-	// Setter method for CityName
-	public void setCityName(String cityName) {
-	    CityName = cityName;
+	public int getId() {
+		return id;
 	}
 }
