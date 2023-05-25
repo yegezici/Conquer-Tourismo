f2d5b1e31b2b3d92a9c8cf88789e5ff2123e9da9
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-26 00:54:37 +0300

    Update NewCenterPane.java

--- .github/workflows/31/NewCenterPane.java
+++ .github/workflows/31/NewCenterPane.java
@@ -116,15 +116,22 @@ public class NewCenterPane extends Pane {
 		createVehicle(lvl);
 
 	}
-
+	//To create a polyline between two points
 	public Polyline createLine(double startX, double startY, double endX, double endY) {
 		Polyline polyline = new Polyline();
+		//If a fixed cell exist on the line
 		int fixed = isThere(startX, startY, endX, endY);
+		//This part is our incomplete part, we start here but we couldn't run it as we want
+		//Therefore; these part almost useless 
+		//I will explain just certain parts
 		if (fixed != 0) {
 			// To determine coordinates of fixed cell
+			//Takes the cordinates of fixed cell
 			double xOfFixed = (fixed % 10 - 1) * cellWidth;
 			double yOfFixed = (fixed / 10 + 1) * cellHeight - 5;
+			//if vehicle's x coordinate bigger than city and y coordinate smaller than
 			if (startX > endX && startY < endY) {
+				//if fixed cell on same column with city
 				if (xOfFixed == endX) {
 					polyline.getPoints().addAll(startX, startY, endX, startY, endX, yOfFixed - 50, endX + 50,
 							yOfFixed - 50);
@@ -134,18 +141,17 @@ public class NewCenterPane extends Pane {
 					} else {
 						// ...
 					}
+					//if fixed cell on same row with vehicle
 				} else if (yOfFixed == startY) {
 					polyline.getPoints().addAll(startX, startY, xOfFixed - 50, startY);
 					fixed = isThere(xOfFixed - 50, startY, endX, endY);
 					if (fixed == 0) {
 						polyline.getPoints().addAll(xOfFixed - 50, endY, endX, endY);
 					}
-					// Bir fixed daha var mi diye bakiyor
 					else {
-						// Burasina devam edilecek
 						xOfFixed = (fixed % 10 - 1) * cellWidth;
 						yOfFixed = (fixed / 10) * cellHeight;
-					}
+					}//After that part is useles but we did not want to delete
 				} else if (yOfFixed == endY) {
 				} else if (xOfFixed == startX) {
 				} else {
@@ -171,55 +177,70 @@ public class NewCenterPane extends Pane {
 			polyline.getPoints().addAll(startX, startY, endX, startY, endX, endY);
 
 		}
+		//Changes the polyline's color and width and returns it
 		polyline.setStroke(Color.GREEN);
 		polyline.setStrokeWidth(7);
 		return polyline;
 
 	}
-
+	//To show the vehicle on the pane
 	public void createVehicle(NewLevel lvl) {
+		//To find vehicle's city id
 		int location = 0;
 		for (int i = 0; i < lvl.cities.size(); i++) {
 			if (lvl.vehicle.getCityId() == lvl.cities.get(i).getId())
 				location = lvl.cities.get(i).getLocId();
 		}
+		//To find its coordinates on the pane
 		int vx = location % 10 - 1;
 		int vy = location / 10;
+		//Resize the imageview of the vehicle
 		lvl.vehicle.imageView.setFitWidth(35);
 		lvl.vehicle.imageView.setFitHeight(35);
 		lvl.vehicle.xCoordinate = vx * cellWidth;
 		lvl.vehicle.yCoordinate = vy * cellHeight;
+		//Put the vehicle on to the pane
 		lvl.vehicle.imageView.setLayoutX(lvl.vehicle.xCoordinate);
 		lvl.vehicle.imageView.setLayoutY(lvl.vehicle.yCoordinate);
 		this.getChildren().add(lvl.vehicle.imageView);
 
 	}
-
+	//To animate the car on a line
 	public void animation(Polyline polyline) {
 		PathTransition transition = new PathTransition();
+		//The duration time of the animation is 2 seconds
 		transition.setDuration(Duration.millis(2000));
+		//Set the path
 		transition.setPath(polyline);
+		//Set the node
 		transition.setNode(lvl.vehicle.imageView);
+		//To take vehicle to the front
 		lvl.vehicle.imageView.toFront();
+		//Just 1 time animation will be displayed
 		transition.setCycleCount(1);
+		//Starts the animation
 		transition.play();
 	}
-
+	//If a fixed cell exist on the line, this method returns its location id
 	public int isThere(double startX, double startY, double endX, double endY) {
 		int fixed = 0;
+		//To check colums
 		ArrayList<Double> x = new ArrayList<>();
+		//To check rows
 		ArrayList<Double> y = new ArrayList<>();
+		//If a fixed cell exist add its coordinates to arraylists
 		for (int i = 0; i < lvl.fixedCells.size(); i++) {
 			x.add((lvl.fixedCells.get(i) % 10 - 1) * cellWidth);
 			y.add((lvl.fixedCells.get(i) / 10) * cellHeight);
 		}
+		//Finally initialize the fixed cell with its location id 
 		for (int i = 0; i < lvl.fixedCells.size(); i++) {
 			if ((x.get(i) <= startX && x.get(i) >= endX) || (x.get(i) >= startX && x.get(i) <= endX)) {
 				if ((y.get(i) <= startY && y.get(i) >= endY) || (y.get(i) >= startY && x.get(i) <= endY)) {
 					fixed = lvl.fixedCells.get(i);
 				}
 			}
-		}
+		}//returns fixed
 		return fixed;
 	}
 
