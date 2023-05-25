0699721760def90ce8a5dda4834b74a69d11abe3
yegezici <120037449+yegezici@users.noreply.github.com>
2023-05-26 01:07:56 +0300

    Update MainPane.java

--- .github/workflows/31/MainPane.java
+++ .github/workflows/31/MainPane.java
@@ -1,8 +1,13 @@
+//Muhammed Hasan Erzincanli 150121031
+//Yunus Emre Gezici 150121066
 import javafx.scene.layout.BorderPane;
 
 public class MainPane extends BorderPane{
+	//This is the top pane of our main pane
 	TopPane top;
+	//This is the center pane of our main pane
 	NewCenterPane center;
+	//This is the bottom pane of our main pane
 	BottomPane bottom;
 	
 	
@@ -11,6 +16,7 @@ public class MainPane extends BorderPane{
 		this.center = center;
 		this.bottom = bottom;
 		
+		//We put them on the pane
 		setTop(top);
 		setCenter(center);
 		setBottom(bottom);
